dca3e055738dc9ac776b5dbc7e8bb3ad83f9bd7f
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-28 20:37:48 +0800

    Update config_flow.py

--- custom_components/openwbmqtt/config_flow.py
+++ custom_components/openwbmqtt/config_flow.py
@@ -1,5 +1,5 @@
 """The openwbmqtt component for controlling the openWB wallbox via home assistant / MQTT"""
-'''from __future__ import annotations
+from __future__ import annotations
 
 from homeassistant.config_entries import ConfigFlow
 
@@ -23,28 +23,4 @@ class openwbmqttConfigFlow(ConfigFlow, domain=DOMAIN):
         return self.async_create_entry(
             title=title,
             data=user_input,
-        )'''
-from homeassistant import config_entries
-from homeassistant.core import callback
-import voluptuous as vol
-
-from .const import DOMAIN
-
-
-class ISENSITFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a ISENSIT config flow."""
-
-    VERSION = 1
-
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
-        if user_input is not None:
-            # Skip validation
-            return self.async_create_entry(title=user_input["sensor_id"], data=user_input)
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {vol.Required("sensor_id"): str}
-            )
         )
